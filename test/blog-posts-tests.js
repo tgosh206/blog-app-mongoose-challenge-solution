8367b79f3b1041bd264c1bbc2b41f93ab717c0a9
Tanner Goshert <tgosh206@hotmail.com>
2018-11-24 14:12:27 -0800

    more initial code added

--- test/blog-posts-tests.js
+++ test/blog-posts-tests.js
@@ -3,6 +3,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const mongoose = require('mongoose');
+const faker = require('faker');
 
 //makes the should syntax for chai available
 const should = chai.should();
@@ -12,3 +13,54 @@ const { closeServer, runServer, app } = require('../server');
 const { TEST_DATABASE_URL } = require('../config');
 
 chai.use(chaiHttp);
+
+// this function deletes the database.
+// it'll be called in an `afterEach` block below
+// to ensure  all data is gone
+function tearDownDb() {
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+
+// used the Faker library from example solution to automatically
+// generate placeholder values and insert that data into mongo
+function seedBlogPostData() {
+  console.info('seeding blog post data');
+  const seedData = [];
+  for (let i = 1; i <= 10; i++) {
+    seedData.push({
+      author: {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName()
+      },
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text()
+    });
+  }
+  // this will return a promise
+  return BlogPost.insertMany(seedData);
+}
+
+describe('blog posts API resource', function () {
+
+  before(function () {
+    return runServer(TEST_DATABASE_URL);
+  });
+
+  beforeEach(function () {
+    return seedBlogPostData();
+  });
+
+  afterEach(function () {
+    // tear down db to be sure no state from this test
+    // effects any ones to come.
+    return tearDownDb();
+  });
+
+  after(function () {
+    return closeServer();
+  });
